cbe5c098459c5588a7740433e44e6f158291f913
huynhhongson <hhongson5011@gmail.com>
2025-08-23 02:02:18 +0700

    Add auto increment id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator')
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose)
+
 const Schema = mongoose.Schema;
 
 
 const CourseSchema = new Schema({
+    _id: {type: Number,},
     name: { type: String, required: true },
     description: { type: String,},
     image: { type: String,},
@@ -12,11 +15,14 @@ const CourseSchema = new Schema({
     level: { type: String,},
     slug: {type: String, slug: 'name', unique: true},
 },{
+    _id: false,
     timestamps: true
 });
 
 //Add plugins
 mongoose.plugin(slug)
+
+CourseSchema.plugin(AutoIncrement)
 CourseSchema.plugin(mongooseDelete, { 
     deletedAt: true,
     overrideMethods: 'all',
